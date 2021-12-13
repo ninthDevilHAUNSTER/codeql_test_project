67027dd7906a6bea8448cd42b5fa9e1a50305ca5
ninthDevilHAUNSTER <luotianhan@126.com>
2021-12-13 11:29:44 +0800

    add task

--- src/main/java/codeql_task/Log4jJndiInjection.java
+++ src/main/java/codeql_task/Log4jJndiInjection.java
@@ -1,5 +1,6 @@
 package codeql_task;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
